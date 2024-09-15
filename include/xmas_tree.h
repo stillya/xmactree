6d08aced157cd05e14f479c35b76d34fb050f845
stillya <st.ilya.101@gmail.com>
2024-09-16 00:38:27 +0500

    polish commit

--- include/xmas_tree.h
+++ include/xmas_tree.h
@@ -1,23 +1,8 @@
 #include <unistd.h>
 
-enum color{
-    RED,
-    GREEN,
-    BLUE,
-    YELLOW
-};
-
-static const char* const COLORS_VAL[] = {
-        [RED] = "\033[0;31m",
-        [GREEN] = "\033[0;32m",
-        [BLUE] = "\033[0;34m",
-        [YELLOW] = "\033[1;33m"
-};
+enum color { RED, GREEN, BLUE, YELLOW };
 
 static const u_int32_t ABSOLUTE_PADDING_LEFT = 80;
 static const u_int32_t ABSOLUTE_PADDING_TOP = 10;
 
-void draw_tree(const char* text);
-
-static void draw_wood(u_int32_t height);
-static enum color rand_color();
+void draw_tree(const char *text);
--- src/xmas_tree.c
+++ src/xmas_tree.c
@@ -4,6 +4,16 @@
 
 #include <xmas_tree.h>
 
+static const char* const COLORS_VAL[] = {
+        [RED] = "\033[0;31m",
+        [GREEN] = "\033[0;32m",
+        [BLUE] = "\033[0;34m",
+        [YELLOW] = "\033[1;33m"
+};
+
+static void draw_wood(u_int32_t height);
+static enum color rand_color();
+
 void draw_tree(const char* text) {
 
     u_int32_t contentLength = strlen(text);
